db1c7e555d8f25ed606b99add191d7faaf804697
riyasklb <riyasklb89@gmail.com>
2023-01-16 12:01:52 +0530

    first commit

---
+++ lib/dart_application_15.dart
@@ -0,0 +1,15 @@
+//import 'package:dart_application_15/dart_application_15.dart'
+// as dart_application_15;
+
+void main(List<String> arguments) {
+  String data = 'Hello';
+  print(data.codeUnits);
+  var n = '\u{0089}';
+  print(n);
+  int a = 2;
+  int b = 1;
+  print('$a + $b = ${a + b}');
+  var c = 1;
+  print(++c);
+  print(a *= b);
+}
---
+++ lib/riya.dart
@@ -0,0 +1,7 @@
+import 'dart:web_gl';
+
+void main(List<String> args) {
+  int a = 1;
+  int b = 1;
+  print({'$a + $b'});
+}
